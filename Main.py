417cc4bdf3d555e7d8243c2fec51b7a271d14149
vescente <138752577+vescente@users.noreply.github.com>
2024-12-29 17:32:53 +0300

    Main py file
    Parsing data from CBR

---
+++ Main.py
@@ -0,0 +1,26 @@
+import requests
+import json
+from flask import Flask
+
+# text = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').text
+
+# curr = json.loads(text)
+
+
+# for currency in curr['Valute']:
+#     print(f"{currency}: {curr['Valute'][currency]['Name']} - {curr['Valute'][currency]['Value']}")
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    text = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').text
+    curr = json.loads(text)
+
+    result = ''
+    for currency in curr['Valute']:
+        result += str(currency) + ': ' + str(curr['Valute'][currency]['Name']) + ' - ' + str(curr['Valute'][currency]['Value']) + '<br>'
+    return result
+
+if __name__ == '__main__':
+    app.run()
